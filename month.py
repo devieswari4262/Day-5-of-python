dfec8b97ff0114af512824579aa9ba74969dc6f1
devi eswari <vanapallidevieswari@gmail.com>
2026-05-29 12:19:16 +0530

    Refactor month.py to include comments and test cases
    Added test cases and comments for membership cost calculation.

--- month.py
+++ month.py
@@ -1,3 +1,18 @@
+#condition statements
+#A gym offeres membership plans based on the number of months enrolled.Write a code tocalculate the total membership cost.
+Test cases:
+Test case1
+input:3
+output:5000
+
+Test case2
+input:5
+output:9000
+
+Test case3
+input:-1
+output:invalid input
+
 Month = int(input("Enter the month:"))
 if Month < 0:
     print("invalid input")
@@ -11,5 +26,12 @@ elif Month == 9:
     print(12000)
 elif Month == 12:
     print(15000)
+
+o/p: Enter the month:3
+5000
+Enter the month:5
+9000
+Enter the month:-1
+invalid input
 else:
     print("error")

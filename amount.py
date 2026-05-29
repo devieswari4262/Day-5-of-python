70d3866c6bab6b56312323c1da29988c4d1d263a
devi eswari <vanapallidevieswari@gmail.com>
2026-05-29 12:24:48 +0530

    Refactor amount.py to improve readability

--- amount.py
+++ amount.py
@@ -11,3 +11,14 @@ else:
 final_amount = amount - (amount * discount)
 print(f"Final amount is {final_amount}")
 print("Final amount after two decimal points:", round(final_amount, 2))
+
+
+o/p:Enter the amount:500
+Final amount is 475.0
+Final amount after two decimal points: 475.0
+Enter the amount:1000
+Final amount is 900.0
+Final amount after two decimal points: 900.0
+Enter the amount:6000
+Final amount is 5100.0
+Final amount after two decimal points: 5100.0
